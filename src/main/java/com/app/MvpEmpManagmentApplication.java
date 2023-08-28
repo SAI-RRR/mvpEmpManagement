8563e02b5bc20a58f4ce1c4bd2914a8087792baa
sai <onlinesvr4@gmail.com>
2023-08-28 10:59:48 +0530

    Main Applciation File name changed

--- src/main/java/com/app/MvpEmpManagmentApplication.java
+++ src/main/java/com/app/MvpEmpManagmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvpLoginPageApplication {
+public class MvpEmpManagmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvpLoginPageApplication.class, args);
+		SpringApplication.run(MvpEmpManagmentApplication.class, args);
 	}
 
 }
